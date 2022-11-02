ebbc049154dc24ba37da1325d049dcf0c2701de1
kurimiaisu <happydogzi812@gmail.com>
2022-11-02 12:37:42 +0300

    first version

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,17 @@
+export default {
+    title: 'SpiritualWorld',
+    description: 'Документация к серверу SpiritualWorld',
+    themeConfig: {
+        sidebar: [
+            {
+                text: 'Начало',
+                items: [
+                        { text: 'Правила', link: '/rules' },
+                    { text: 'Что нового?', link: '/new-features' },
+                    { text: 'Модификации', link: '/modifications' }
+                ],
+            }
+        ]
+    },
+    base: '/spiritualworld-docs/'
+}
